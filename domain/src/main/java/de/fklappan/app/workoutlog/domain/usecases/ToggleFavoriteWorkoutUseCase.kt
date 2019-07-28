3c375d8675dbe0bde92b3799ef4dbc32284abaf3
Florian Klappan <florian.klappan@googlemail.com>
2019-07-29 00:17:14 +0200

    Never trust android studio...

--- domain/src/main/java/de/fklappan/app/workoutlog/domain/usecases/ToggleFavoriteWorkoutUseCase.kt
+++ domain/src/main/java/de/fklappan/app/workoutlog/domain/usecases/ToggleFavoriteWorkoutUseCase.kt
@@ -7,7 +7,7 @@ import io.reactivex.Single
 class ToggleFavoriteWorkoutUseCase(val repository: WorkoutLogRepository) : UseCase<Int,WorkoutDomainModel> {
 
     override fun execute(workoutId: Int): Single<WorkoutDomainModel> {
-        return Single.create
+        return Single.create<WorkoutDomainModel>
         {
             val workoutDomainModel = repository.getWorkoutById(workoutId)
             workoutDomainModel.favorite = !workoutDomainModel.favorite
