76adaa0b38f0e08495472aa4694645b026dce223
Florian Klappan <florian.klappan@googlemail.com>
2019-07-28 22:45:57 +0200

    fixed warnings

--- app/src/main/java/de/fklappan/app/workoutlog/ui/overviewworkout/OverviewWorkoutFragment.kt
+++ app/src/main/java/de/fklappan/app/workoutlog/ui/overviewworkout/OverviewWorkoutFragment.kt
@@ -23,7 +23,7 @@ class OverviewWorkoutFragment : BaseFragment() {
     private lateinit var overviewWorkoutAdapter: OverviewWorkoutAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.overview, container, false)
+        return inflater.inflate(R.layout.overview, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
