4e0fbfaef56f5a3e55c29931866820d2bf0a4eeb
Florian Klappan <florian.klappan@googlemail.com>
2019-07-29 22:09:28 +0200

    removed unused class

--- app/src/main/java/de/fklappan/app/workoutlog/ui/detailview/DetailviewWorkoutState.kt
+++
@@ -1,7 +0,0 @@
-package de.fklappan.app.workoutlog.ui.detailview
-
-data class DetailviewWorkoutState (
-    val workout : WorkoutDetailsGuiModel?,
-    val error: Throwable?,
-    val loading: Boolean
-)
