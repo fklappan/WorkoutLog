da805f32a763f85e7d23c219b08fe05c6f02f5d1
Florian Klappan <florian.klappan@googlemail.com>
2021-09-26 20:40:37 +0200

    Closes #33 remove old PRs if a new one is achieved

--- domain/src/main/java/de/fklappan/app/workoutlog/domain/usecases/AddWorkoutResultUseCase.kt
+++ domain/src/main/java/de/fklappan/app/workoutlog/domain/usecases/AddWorkoutResultUseCase.kt
@@ -1,14 +1,24 @@
 package de.fklappan.app.workoutlog.domain.usecases
 
-import de.fklappan.app.workoutlog.domain.WorkoutDomainModel
 import de.fklappan.app.workoutlog.domain.WorkoutLogRepository
 import de.fklappan.app.workoutlog.domain.WorkoutResultDomainModel
 import io.reactivex.Completable
 
 class AddWorkoutResultUseCase(val repository: WorkoutLogRepository) : OutputlessUseCase<WorkoutResultDomainModel> {
     override fun execute(param: WorkoutResultDomainModel): Completable {
+        return Completable.fromCallable { internalExecute(param) }
+    }
+
+    private fun internalExecute(param: WorkoutResultDomainModel) {
+        if (param.pr) {
+            // remove pr from old results
+            val results = repository.getResultsForWorkout(param.workout)
+            results.filter { it.pr }.forEach {
+                it.pr = false
+                repository.updateResult(it)
+            }
+        }
         repository.addResult(param.workout, param)
-        return Completable.complete()
     }
 
 }
--- domain/src/main/java/de/fklappan/app/workoutlog/domain/usecases/EditResultUseCase.kt
+++ domain/src/main/java/de/fklappan/app/workoutlog/domain/usecases/EditResultUseCase.kt
@@ -6,8 +6,19 @@ import io.reactivex.Completable
 
 class EditResultUseCase(val repository: WorkoutLogRepository) : OutputlessUseCase<WorkoutResultDomainModel> {
     override fun execute(param: WorkoutResultDomainModel): Completable {
+        return Completable.fromCallable { internalExecute(param)}
+    }
+
+    private fun internalExecute(param: WorkoutResultDomainModel) {
+        if (param.pr) {
+            // remove pr from old results
+            val results = repository.getResultsForWorkout(param.workout)
+            results.filter { it.pr }.forEach {
+                it.pr = false
+                repository.updateResult(it)
+            }
+        }
         repository.updateResult(param)
-        return Completable.complete()
     }
 
 }
